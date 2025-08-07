fc41131fc25d24e3bc17a009f58b90a3c14eb378
Jessevitec <jesse.lagland@vitecsoftware.com>
2025-08-07 11:56:50 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,38 @@
-// Scripts for firebase and firebase messaging
-// eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app-compat.js');
-// eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging-compat.js')
+// firebase-messaging-sw.js
 
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 // Your Firebase configuration
-// eslint-disable-next-line no-undef
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyARSGdBCb1A3rYDV_3lRjr6bPLJROq6Fic",
     authDomain: "testproject-5ce7b.firebaseapp.com",
     projectId: "testproject-5ce7b",
     storageBucket: "testproject-5ce7b.firebasestorage.app",
     messagingSenderId: "655770773574",
-    appId: "1:655770773574:web:ba6bf259ec34396d628247"
-});
+    appId: "1:655770773574:web:de18fa8cbfb9ebc9628247"
+};
 
-// Initialize Firebase
+// Initialize Firebase in the service worker
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
+
 // Retrieve Firebase Messaging instance
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
 // Handle background messages
-messaging.onBackgroundMessage(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+messaging.onBackgroundMessage((payload) => {
+    console.log('📦 Received background message:', payload);
+
+/*    const notificationTitle = payload.notification?.title || 'Default Title';
+    const notificationOptions = {
+        body: payload.notification?.body || 'Default Body',
+        icon: payload.notification?.icon || '/default-icon.png'
+    };
+
+    // eslint-disable-next-line no-restricted-globals
+    self.registration.showNotification(notificationTitle, notificationOptions);*/
 });
-
-
