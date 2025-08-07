d76d67ff1ca50e27960f49ffde81161adfddbba0
Jessevitec <jesse.lagland@vitecsoftware.com>
2025-08-07 10:34:33 +0300

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,38 @@
+// Scripts for firebase and firebase messaging
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts(
+    "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+const firebaseConfig = {
+    apiKey: "AIzaSyARSGdBCb1A3rYDV_3lRjr6bPLJROq6Fic",
+    authDomain: "testproject-5ce7b.firebaseapp.com",
+    projectId: "testproject-5ce7b",
+    storageBucket: "testproject-5ce7b.firebasestorage.app",
+    messagingSenderId: "655770773574",
+    appId: "1:655770773574:web:de18fa8cbfb9ebc9628247"
+};
+
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
+
+// eslint-disable-next-line no-undef
+firebase.setLogLevel('debug');
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('test');
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image,
+    };
+
+    // eslint-disable-next-line no-restricted-globals
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
