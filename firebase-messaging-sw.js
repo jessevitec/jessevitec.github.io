190d8110d83534e204dea8521e09abd05d85e839
Jessevitec <jesse.lagland@vitecsoftware.com>
2025-08-07 11:02:45 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,29 @@
 // Scripts for firebase and firebase messaging
 // eslint-disable-next-line no-undef
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app-compat.js');
 // eslint-disable-next-line no-undef
-importScripts(
-    "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging-compat.js')
+
+
+// Your Firebase configuration
+// eslint-disable-next-line no-undef
+firebase.initializeApp({
+    apiKey: "AIzaSyARSGdBCb1A3rYDV_3lRjr6bPLJROq6Fic",
+    authDomain: "testproject-5ce7b.firebaseapp.com",
+    projectId: "testproject-5ce7b",
+    storageBucket: "testproject-5ce7b.firebasestorage.app",
+    messagingSenderId: "655770773574",
+    appId: "1:655770773574:web:ba6bf259ec34396d628247"
+});
+
+// Initialize Firebase
+// Retrieve Firebase Messaging instance
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+});
+
+
